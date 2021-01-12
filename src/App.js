57c79d36d90d69edd87d0c8244b7dfc333979348
EstagioAutomacao <estagiodaautomacao@gmail.com>
2021-01-12 11:51:32 -0300

    Front-end YouTube atualizado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core";
 
 import Home from "./Home";
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
   const theme = createMuiTheme({
-    spacing: 1,
+    spacing: 4,
     palette: {
+      type: darkMode ? "dark" : "light",
       primary: {
         main: "#4a148c",
       },
       secondary: {
-        main: "#3f51b5",
+        main: "#3ea6ff",
+        main2: "#3f51b5",
+      },
+      background: {
+        default: darkMode ? "#232323" : "#fff",
+        dark: darkMode ? "#181818" : "#f4f6f8",
+        paper: darkMode ? "#232323" : "#fff",
       },
     },
   });
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <Home darkMode={darkMode} setDarkMode={setDarkMode} />
     </ThemeProvider>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -5,20 +5,56 @@ import {
   Toolbar,
   IconButton,
   Button,
+  Drawer,
+  List,
+  ListItem,
+  Divider,
+  ListItemText,
+  ListItemIcon,
+  ListSubheader,
+  Box,
+  Typography,
+  Grid,
+  Hidden,
+  Switch,
 } from "@material-ui/core";
 
+import { useTheme } from "@material-ui/core/styles";
+
+//Icons
 import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import VideoCallIcon from "@material-ui/icons/VideoCall";
 import AppsIcon from "@material-ui/icons/Apps";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import HomeIcon from "@material-ui/icons/Home";
+import WhatshotIcon from "@material-ui/icons/Whatshot";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import HistoryIcon from "@material-ui/icons/History";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+
+import Videos from "./Videos";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
+    backgroundColor: theme.palette.background.dark,
   },
   appBar: {
     boxShadow: "none",
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  logo: {
+    height: 25,
+  },
+  drawer: {
+    width: 240,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: 240,
+    borderRight: "none",
   },
   menuIcon: {
     paddingRight: theme.spacing(5),
@@ -30,28 +66,45 @@ const useStyles = makeStyles((theme) => ({
   grow: {
     flexGrow: 1,
   },
+  listItemText: {
+    fontSize: 14,
+  },
+  listItem: {
+    paddingTop: 4,
+    paddingBottom: 4,
+  },
+  subHeader: {
+    textTransform: "uppercase",
+  },
 }));
-export default function Home() {
+export default function Home({ darkMode, setDarkMode }) {
   const classes = useStyles();
+  const theme = useTheme();
   return (
     <div className={classes.root}>
       <AppBar color="inherit" className={classes.appBar}>
         <Toolbar>
-          <IconButton
-            className={classes.menuIcon}
-            color="inherit"
-            aria-label="menu"
-          >
+          <IconButton className={classes.menuIcon} aria-label="menu">
             <MenuIcon />
           </IconButton>
+          <img
+            src={
+              theme.palette.type === "dark"
+                ? "/images/branco.png"
+                : "/images/preto.png"
+            }
+            alt="logo"
+            className={classes.logo}
+          />
           <div className={classes.grow} />
-          <IconButton className={classes.icons} color="inherit">
+          <Switch value={darkMode} onChange={() => setDarkMode(!darkMode)} />
+          <IconButton className={classes.icons}>
             <VideoCallIcon />
           </IconButton>
-          <IconButton className={classes.icons} color="inherit">
+          <IconButton className={classes.icons}>
             <AppsIcon />
           </IconButton>
-          <IconButton className={classes.icons} color="inherit">
+          <IconButton className={classes.icons}>
             <MoreVertIcon />
           </IconButton>
           <Button
@@ -63,6 +116,158 @@ export default function Home() {
           </Button>
         </Toolbar>
       </AppBar>
+      <Box display="flex">
+        <Hidden mdDown>
+          <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            <Toolbar />
+            <div className={classes.drawerContainer}>
+              <List>
+                {[
+                  ["Início", <HomeIcon />],
+                  ["Em alta", <WhatshotIcon />],
+                  ["Incrições", <SubscriptionsIcon />],
+                ].map((text, index) => (
+                  <ListItem
+                    button
+                    classes={{
+                      root: classes.listItem,
+                    }}
+                    key={text}
+                  >
+                    <ListItemIcon>{text[1]}</ListItemIcon>
+                    <ListItemText
+                      classes={{
+                        primary: classes.listItemText,
+                      }}
+                      primary={text[0]}
+                    />
+                  </ListItem>
+                ))}
+              </List>
+              <Divider />
+              <List>
+                {[
+                  ["Biblioteca", <VideoLibraryIcon />],
+                  ["Histórico", <HistoryIcon />],
+                ].map((text, index) => (
+                  <ListItem
+                    button
+                    classes={{
+                      root: classes.listItem,
+                    }}
+                    key={text}
+                  >
+                    <ListItemIcon>{text[1]}</ListItemIcon>
+                    <ListItemText
+                      classes={{
+                        primary: classes.listItemText,
+                      }}
+                      primary={text[0]}
+                    />
+                  </ListItem>
+                ))}
+              </List>
+              <Divider />
+              <Box p={7}>
+                <Typography variant="body2">
+                  Faça login para curtir vídeos, comentar e se inscrever.
+                </Typography>
+                <Box mt={2}>
+                  <Button
+                    startIcon={<AccountCircle />}
+                    color="secondary"
+                    variant="outlined"
+                  >
+                    Fazer Login
+                  </Button>
+                </Box>
+              </Box>
+              <Divider />
+              <List
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+                subheader={
+                  <ListSubheader
+                    component="div"
+                    id="nested-list-subheader"
+                    className={classes.subHeader}
+                  >
+                    O melhor do YouTube
+                  </ListSubheader>
+                }
+              >
+                {[
+                  "Música",
+                  "Esportes",
+                  "Jogos",
+                  "Filmes",
+                  "Notícias",
+                  "Ao vivo",
+                  "Aprender",
+                  "Vídeos do momento",
+                  "Vídeo em 360",
+                ].map((text, index) => (
+                  <ListItem button key={text}>
+                    <ListItemIcon>
+                      <AddCircleIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItem>
+                ))}
+              </List>
+            </div>
+          </Drawer>
+        </Hidden>
+        <Box p={8}>
+          <Toolbar />
+          <Typography
+            variant="h5"
+            color="textPrimary"
+            style={{ fontWeight: 600 }}
+          >
+            Recomendados
+          </Typography>
+          <Grid container spacing={4}>
+            {Videos.map((item, index) => (
+              <Grid key={index} item lg={3} md={4} sm={6} xs={12}>
+                <Box>
+                  <img
+                    style={{ width: "100%" }}
+                    alt={item.title}
+                    src={item.thumb}
+                  />
+                  <Box>
+                    <Typography
+                      style={{ fontWeight: 600 }}
+                      gutterBottom
+                      variant="body1"
+                      color="textPrimary"
+                    >
+                      {item.title}
+                    </Typography>
+                    <Typography
+                      display="block"
+                      variant="body2"
+                      color="textSecondary"
+                    >
+                      {item.channel}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      {`${item.views} - ${item.date}`}
+                    </Typography>
+                  </Box>
+                </Box>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Box>
     </div>
   );
 }
